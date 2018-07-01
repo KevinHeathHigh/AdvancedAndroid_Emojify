cca29c90e73f45935db08ba6847de173b04a00bf
kevinheathhigh <kevin@hobbitsoft.net>
2018-07-01 13:08:57 -0700

    Finished Exercise: Detect Faces Intro

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,32 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+public class Emojifier {
+
+    private static final String TAG = Emojifier.class.getSimpleName();
+
+    public static void detectFaces(Context context, Bitmap bitmap) {
+
+        FaceDetector faceDetector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = faceDetector.detect(frame);
+
+        Log.d(TAG, "#############################################################");
+        Log.d(TAG, "Number of faces detected: " + String.valueOf(faces.size()));
+        Log.d(TAG, "#############################################################");
+    }
+}
